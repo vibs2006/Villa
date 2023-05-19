a99b276d5f703f380fe07199d306f7c8619a5e7f
Vibs <vibs2006@gmail.com>
2023-05-19 23:14:47 +0530

    Basic API with HTTP Get Method from start.

---
+++ VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaApi.Models;
+
+namespace VillaApi.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> { 
+            new Villa { Id = 1, Name = "Pool View"},
+            new Villa { Id = 2, Name = "Beach View"}            
+            };
+
+        }
+    }
+}
---
+++ VillaApi/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿namespace VillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        
+    }
+}
